791ee892397ea84e5667a00575a9cf12d0774cbe
Saisandeepsangeetham <sandeep.sangeetham845@gmail.com>
2023-08-13 05:10:07 +0530

    face_detect

--- Face_Detect/face_detect.py
+++ Face_Detect/face_detect.py
@@ -4,7 +4,7 @@ img = cv.imread("Images\images.jpeg")
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-haar_cascade = cv.CascadeClassifier('haar_cascade.xml')
+haar_cascade = cv.CascadeClassifier('Face_detect/haar_cascade.xml')
 
 face_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1,minNeighbors=3)
 
